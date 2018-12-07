5d3b868efb56480c4c8de05509065eb7bf9c6a6b
tracedence <shoaibrashid887@gmail.com>
2018-12-07 13:28:46 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -7,5 +8,18 @@ from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
 
 
-# Your solution code here
+# Your solution code hered
+def rf_rfe(df):
+    
+    x = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+    #creating a model
+    Ra = RandomForestClassifier()
+    rf = RFE(Ra)
+    #
+    rf.fit(x,y) 
+    most_sig = list(x.columns[rf.support_])
+    return most_sig
+rf_rfe(data)
+
 
